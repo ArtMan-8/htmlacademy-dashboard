0dc74bd0048bce9df649e726d61eecd42e728d25
Art <ya.Artriches@ya.ru>
2022-09-18 21:56:47 +0300

    add: projects

--- src/constants.ts
+++ src/constants.ts
@@ -44,4 +44,39 @@ export const Courses: ICourses = {
     description: 'React. Разработка сложных клиентских приложений',
     projects: ['six-cities', 'what-to-watch'],
   },
+  vue: {
+    organization: `htmlacademy-vue`,
+    description: 'Vue. Разработка клиентских приложений',
+    projects: ['pizza'],
+  },
+  nodejs: {
+    organization: `htmlacademy-nodejs`,
+    description: 'Node. Профессиональная разработка REST API',
+    projects: ['typoteka', 'buy-and-sell'],
+  },
+  php1: {
+    organization: `htmlacademy-php`,
+    description: 'PHP. Профессиональная веб-разработка',
+    projects: ['readme', 'doingsdone', 'yeticave'],
+  },
+  php2: {
+    organization: `htmlacademy-yii`,
+    description: 'PHP и Yii. Архитектура сложных веб-сервисов',
+    projects: ['task-force'],
+  },
+  php3: {
+    organization: `htmlacademy-php3`,
+    description: 'PHP и Laravel. Создание современных бэкендов',
+    projects: ['what-to-watch'],
+  },
+  animation: {
+    organization: `htmlacademy-animation`,
+    description: 'Анимация для фронтендеров',
+    projects: ['magic-vacation'],
+  },
+  email: {
+    organization: `htmlacademy-email`,
+    description: 'Вёрстка email-рассылок',
+    projects: ['play-html'],
+  },
 };
